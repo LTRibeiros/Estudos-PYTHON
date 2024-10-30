77349b5db0a8d2e1f0d9382d43f9a8e9d15b2133
Gabriel Ribeiro <chucrutsgabriel@gmail.com>
2024-10-29 23:02:37 -0300

    Add files via upload

---
+++ Desenvolvimento_Orientado_a_Objetos/Cliente.py
@@ -0,0 +1,20 @@
+class Cliente:
+    def __init__(self, Nome, Cod, Num):
+
+     self._NomeUsuario = Nome
+     self._CodigoUsuario = Cod
+     self._NumeroUsuario = Num
+
+#metodo get
+def get_NomeUsuario(self):
+    return self._NomeUsuario
+
+def get_CodigoUsuario(self):
+    return self._NomeUsuario
+
+def get_NumeroUsuario(self):
+    return self._NomeUsuario
+
+
+
+
---
+++ Desenvolvimento_Orientado_a_Objetos/MainClass.py
@@ -0,0 +1,12 @@
+class Main:
+    pass
+print("Testando Novo Projeto")
+
+from Cliente import Cliente
+
+c1 = Cliente("Gabriel", 505, "11 979548787")
+
+print(c1)
+print(c1._NomeUsuario , c1._CodigoUsuario , c1._NumeroUsuario)
+
+
